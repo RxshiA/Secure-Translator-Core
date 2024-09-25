f01242065497fec579f3f60dff4a49c1916ed8a1
Nethmi <it21256196@my.sliit.lk>
2024-09-25 22:42:52 +0530

    Define allowed origins

--- server/server.js
+++ server/server.js
@@ -17,6 +17,18 @@ app.disable('x-powered-by');
 const allowedOrigins = ['http://localhost:3000'];
 const URL = process.env.MONGODB_URL;
 
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 mongoose.connect(URL, {
   useNewUrlParser: true,
 });
