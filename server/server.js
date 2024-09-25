fa781b02cd52361c884e8f2cb9849c4c0691901c
Nethmi <it21256196@my.sliit.lk>
2024-09-25 22:35:45 +0530

    Disable the X-Powered-By header

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const helmet = require("helmet"); 
 const app = express();
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8070;
 
-app.use(cors());
-app.use(bodyParser.json());
+// Disable the X-Powered-By header
+app.disable('x-powered-by');
 
 const URL = process.env.MONGODB_URL;
 
