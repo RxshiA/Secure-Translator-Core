5de5808c3ee52f4d3f12e7c176dd726a1c6ae227
Rashmika Fernando <sethumrashmika3001@gmail.com>
2024-09-26 20:31:08 +0530

    feat: OAuth-rashmika

--- client/src/components/SignupFlow/Login.js
+++ client/src/components/SignupFlow/Login.js
@@ -1,6 +1,9 @@
+/* eslint-disable no-unused-vars */
 import React, { Component } from 'react'
 import { login } from '../../api/api'
 import BrowserStorage from '../../helper/BrowserStorage';
+import { doSignInWithEmailAndPassword, doSignInWithGoogle } from '../../firebase/auth'
+import { useAuth } from '../../contexts/authContext'
 
 export default class Login extends Component {
     constructor(props) {
@@ -28,6 +31,13 @@ export default class Login extends Component {
                 }
             });
     };
+  onGoogleSignIn = (e) => {
+        e.preventDefault()
+       
+            doSignInWithGoogle()
+          
+        }
+    
     render() {
         return (
             <div className="min-h-screen bg-no-repeat bg-cover bg-center"
@@ -59,11 +69,25 @@ export default class Login extends Component {
                                 <div className="">
                                     <button type="submit" className="mt-4 mb-3 w-full bg-green-500 hover:bg-green-400 text-white py-2 rounded-md transition duration-100">
                                         Login now</button>
-                                    <div className="flex  space-x-2 justify-center items-end bg-gray-700 hover:bg-gray-600 text-white py-2 rounded-md transition duration-100">
-
-                                        <img className=" h-5 cursor-pointer" src="https://i.imgur.com/arC60SB.png" alt="" />
-                                        <button >Or sign-in with google</button>
-                                    </div>
+                                        <button
+                      
+                        onClick={(e) => { this.onGoogleSignIn(e) }}
+                        className='w-full flex items-center justify-center gap-x-3 py-2.5 border rounded-lg text-sm font-medium hover:bg-gray-100 transition duration-300 active:bg-gray-100'>
+                        <svg className="w-5 h-5" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g clipPath="url(#clip0_17_40)">
+                                <path d="M47.532 24.5528C47.532 22.9214 47.3997 21.2811 47.1175 19.6761H24.48V28.9181H37.4434C36.9055 31.8988 35.177 34.5356 32.6461 36.2111V42.2078H40.3801C44.9217 38.0278 47.532 31.8547 47.532 24.5528Z" fill="#4285F4" />
+                                <path d="M24.48 48.0016C30.9529 48.0016 36.4116 45.8764 40.3888 42.2078L32.6549 36.2111C30.5031 37.675 27.7252 38.5039 24.4888 38.5039C18.2275 38.5039 12.9187 34.2798 11.0139 28.6006H3.03296V34.7825C7.10718 42.8868 15.4056 48.0016 24.48 48.0016Z" fill="#34A853" />
+                                <path d="M11.0051 28.6006C9.99973 25.6199 9.99973 22.3922 11.0051 19.4115V13.2296H3.03298C-0.371021 20.0112 -0.371021 28.0009 3.03298 34.7825L11.0051 28.6006Z" fill="#FBBC04" />
+                                <path d="M24.48 9.49932C27.9016 9.44641 31.2086 10.7339 33.6866 13.0973L40.5387 6.24523C36.2 2.17101 30.4414 -0.068932 24.48 0.00161733C15.4055 0.00161733 7.10718 5.11644 3.03296 13.2296L11.005 19.4115C12.901 13.7235 18.2187 9.49932 24.48 9.49932Z" fill="#EA4335" />
+                            </g>
+                            <defs>
+                                <clipPath id="clip0_17_40">
+                                    <rect width="48" height="48" fill="white" />
+                                </clipPath>
+                            </defs>
+                        </svg>
+                       
+                    </button>
                                 </div>
                             </form>
                             <p className="mt-8"> Dont have an account? <span className="cursor-pointer text-sm text-blue-600"> <a href="/register">Join free today</a></span></p>
