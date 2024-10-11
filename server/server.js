4d044d10f55adf86604980dc42c58e73462a1b23
Yasanjith Rajapathirane <99582488+IT21256196-WE@users.noreply.github.com>
2024-10-11 23:09:25 +0530

    Server.js : Meta data reroute,Headers Secured,Allowed orgins added

--- server/server.js
+++ server/server.js
@@ -13,31 +13,9 @@ const PORT = process.env.PORT || 8070;
 // Disable the X-Powered-By header
 app.disable('x-powered-by');
 
-// Define allowed origins
-const allowedOrigins = ['http://localhost:3000'];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
 
-app.use(bodyParser.json());
 
-// Middleware to block access to cloud metadata IP address
-app.use((req, res, next) => {
-  if (req.ip === '169.254.169.254') {
-    res.status(403).send('Access denied');
-  } else {
-    next();
-  }
-});
 
 // Middleware to restrict access to sensitive files
 app.use((req, res, next) => {
@@ -72,6 +50,43 @@ app.use((req, res, next) => {
 
 // Use Helmet to set security headers
 app.use(helmet());
+app.disable('x-powered-by');
+app.disable('x-forwarded-for');
+
+// Middleware for removing sensitive headers
+app.use((req, res, next) => {
+  res.removeHeader('Date'); // Remove Date header
+  res.removeHeader('Last-Modified'); // Remove Last-Modified header
+  next();
+});
+
+// Define allowed origins for CORS
+const allowedOrigins = ['http://localhost:3000'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
+
+app.use(bodyParser.json());
+
+// Middleware to block requests to the cloud metadata service
+app.use((req, res, next) => {
+  if (req.hostname === '169.254.169.254' || req.get('Host') === '169.254.169.254') {
+    res.status(403).send('Access denied');
+  } else {
+    next();
+  }
+});
+
 
 // Set X-Frame-Options header to DENY
 app.use(helmet.frameguard({ action: 'deny' }));
